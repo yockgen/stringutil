13b50acafb1a938f60c752c26e045b017fb7fe51
yockgen <yockgen@gmail.com>
2022-08-23 11:52:40 +0800

    Update reverse.go

--- reverse.go
+++ reverse.go
@@ -1,6 +1,6 @@
 package stringutil
 
-func Reverse(s string) string {
+func Reverse01(s string) string {
 	r := []rune(s)
 	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
 		r[i], r[j] = r[j], r[i]
@@ -8,7 +8,7 @@ func Reverse(s string) string {
 	return string(r)
 }
 
-func test01 (s string) string {
+func Test01 (s string) string {
 	r := []rune(s)
 	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
 		r[i], r[j] = r[j], r[i]
